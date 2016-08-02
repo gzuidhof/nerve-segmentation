7b338bb57edad6efa564f51ccf3f81b463052d9d
Guido Zuidhof <me@guido.io>
2016-08-02 11:27:03 +0200

    Generalize nerve only copy

--- src/cp_with_nerve.py
+++ src/cp_with_nerve.py
@@ -1,17 +1,20 @@
 import numpy as np
 import glob
-from params import params as P
 import dataset
 import scipy.misc
 from shutil import copyfile
+import util
 
 if __name__ == "__main__":
-    filenames = glob.glob(P.FILENAMES_TRAIN)
+    filenames = glob.glob('../data/train_smaller/*.tif')
     filenames = filter(lambda x: 'mask' not in x, filenames)
+    util.make_dir_if_not_present('../data/train_smaller_nonempty')
+
+    name='smaller'
 
     for f in filenames:
         mask_f = dataset.to_mask_path(f)
 
         if np.sum(scipy.misc.imread(mask_f)) > 0:
-            copyfile(f, f.replace('train_small','train_small_nonempty'))
-            copyfile(mask_f, mask_f.replace('train_small','train_small_nonempty'))
+            copyfile(f, f.replace('train_'+name,'train_'+name+'_nonempty'))
+            copyfile(mask_f, mask_f.replace('train_'+name,'train_'+name+'_nonempty'))
